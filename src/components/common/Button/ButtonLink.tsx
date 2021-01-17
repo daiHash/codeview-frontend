0b13593ac7858fa55b6220e5598e83b48a6757fa
Daichi Hashimura <dhashi92@gmail.com>
2021-01-18 02:18:52 +0900

    Add button link

---
+++ src/components/common/Button/ButtonLink.tsx
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+import React, { Fragment } from "react";
+import Link from "next/link";
+
+type Props = {
+  href: string;
+  isExternal?: boolean;
+  invert?: boolean;
+};
+
+export const ButtonLink: React.FC<Props> = ({
+  children,
+  href,
+  isExternal,
+  invert,
+}) => {
+  return (
+    <Fragment>
+      {isExternal ? (
+        <A
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          invert={invert}
+        >
+          {children}
+        </A>
+      ) : (
+        <Link href={href}>
+          <A invert={invert} tabIndex={0}>
+            {children}
+          </A>
+        </Link>
+      )}
+    </Fragment>
+  );
+};
+
+const A = styled.a<{ invert?: boolean }>`
+  display: inline-block;
+  text-align: center;
+  background: ${({ invert }) => (invert ? "transparent" : "#4568fb")};
+  color: ${({ invert }) => (invert ? "#4568fb" : "#fff")};
+  border-radius: 4px;
+  width: auto;
+  min-width: 120px;
+  padding: 10px 15px;
+  font-size: 18px;
+  border: ${({ invert }) => (invert ? "1px solid #4568fb" : "none")};
+  cursor: pointer;
+  transition: background 0.2s color 0.2s;
+
+  &:hover {
+    background: ${({ invert }) =>
+      invert ? "#4568fb" : "rgba(69, 105, 251, 0.9)"};
+    color: #fff;
+  }
+`;
