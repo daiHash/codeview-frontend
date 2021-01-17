badb3198e3e27964cb6ec13096e0698181bbc5ab
Daichi Hashimura <dhashi92@gmail.com>
2021-01-18 02:18:41 +0900

    Add hamburger content

---
+++ src/components/common/Header/HamburgerMenu.tsx
@@ -0,0 +1,100 @@
+import styled from "@emotion/styled";
+import React, { Fragment } from "react";
+import HamburgerIconAsset from "./assets/Hamburger-Icon.svg";
+import Image from "next/image";
+import { TextLink } from "../TextLink";
+
+export const HamburgerMenu: React.FC = () => {
+  return (
+    <Fragment>
+      <MenuButton
+        aria-controls="global-nav"
+        aria-haspopup="true"
+        id="menu-button"
+      >
+        <Image
+          src={HamburgerIconAsset}
+          alt=""
+          width={20}
+          height={15}
+          aria-hidden
+        />
+        <span>MENU</span>
+      </MenuButton>
+
+      <MenuContent id="global-nav" aria-label="Main navigation">
+        <ul>
+          <li>
+            <TextLink href="/profile" isRouterLink>
+              Profile
+            </TextLink>
+          </li>
+          <li>
+            <TextLink href="/snippet/favorites" isRouterLink>
+              Favorite Snippets
+            </TextLink>
+          </li>
+          <li>
+            <TextLink href="/profile/my-snippets" isRouterLink>
+              My Snippets
+            </TextLink>
+          </li>
+          <li>
+            <TextLink href="http://localhost:3000/api/auth/logout">
+              Log out
+            </TextLink>
+          </li>
+        </ul>
+      </MenuContent>
+    </Fragment>
+  );
+};
+
+const MenuButton = styled.button`
+  margin-left: 40px;
+
+  span {
+    display: block;
+  }
+`;
+
+const MenuContent = styled.nav`
+  position: absolute;
+  top: 80px;
+  right: 10px;
+  background: #fff;
+  border-radius: 2px;
+  border: 1px solid rgba(48, 48, 48, 0.2);
+
+  ul {
+    li {
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+      transition: background 0.2s;
+
+      &:last-of-type {
+        margin-top: 10px;
+        &::before {
+          display: block;
+          content: "";
+          width: 80%;
+          height: 1px;
+          margin: 0 auto;
+          background: rgba(48, 48, 48, 0.15);
+        }
+      }
+
+      &:hover {
+        background: rgba(48, 48, 48, 0.15);
+      }
+
+      a {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        padding: 0 20px;
+      }
+    }
+  }
+`;
