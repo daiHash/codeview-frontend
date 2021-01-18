9f9fce044bd5a82882c8584bba2b5a48a72c0fb1
Daichi Hashimura <dhashi92@gmail.com>
2021-01-19 01:11:17 +0900

    Add signin component

---
+++ src/components/common/Header/SignInMenu.tsx
@@ -0,0 +1,54 @@
+import React, { Fragment } from "react";
+import { ButtonLink } from "../Button/ButtonLink";
+import Image from "next/image";
+import GoogleSignInButtonAsset from "./assets/btn_google_signin.png";
+import styled from "@emotion/styled";
+import { TextLink } from "../TextLink";
+import { useToggle } from "utils/hooks/useToggle";
+import { Button } from "../Button";
+
+export const SignInMenu = () => {
+  const [isOpen, toggle] = useToggle(false);
+
+  return (
+    <Fragment>
+      <Button onClick={toggle}>Sign In</Button>
+
+      {isOpen && (
+        <MenuContent>
+          <ul>
+            <li>
+              <TextLink href="http://localhost:3000/api/auth/google">
+                <Image
+                  src={GoogleSignInButtonAsset}
+                  alt="Google Sign In Button"
+                  width={191}
+                  height={46}
+                />
+              </TextLink>
+            </li>
+          </ul>
+        </MenuContent>
+      )}
+    </Fragment>
+  );
+};
+
+const MenuContent = styled.nav`
+  position: absolute;
+  top: 80px;
+  right: 10px;
+  background: #fff;
+  border-radius: 2px;
+  border: 1px solid rgba(48, 48, 48, 0.2);
+
+  ul {
+    padding: 0 20px;
+
+    li {
+      width: 100%;
+      height: 50px;
+      line-height: 40px;
+    }
+  }
+`;
