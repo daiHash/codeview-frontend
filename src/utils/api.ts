e5a136cd7c767ff3ad2e1f4f0285fe7d30daaac5
Daichi Hashimura <dhashi92@gmail.com>
2021-01-31 18:14:50 +0900

    Fix URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,14 +2,19 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: process.env.API_URL,
-  // baseURL: "http://localhost:3000/api",
-  baseURL: "https://code-snippet-memo.herokuapp.com/api",
+  baseURL:
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000/api"
+      : "https://code-snippet-memo.herokuapp.com/api",
   xsrfHeaderName: "X-CSRF-Token",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin":
+      process.env.NODE_ENV !== "production"
+        ? "http://localhost:8080"
+        : "https://code-snippet-memo-frontend-ekjiz8j5t.vercel.app/",
   },
   responseType: "json",
 });
