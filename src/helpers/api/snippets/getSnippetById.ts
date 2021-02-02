7babf713c948a1477e483525ab2e49081c20c242
Daichi Hashimura <dhashi92@gmail.com>
2021-02-03 03:11:15 +0900

    Add helper to get snippet by id

---
+++ src/helpers/api/snippets/getSnippetById.ts
@@ -0,0 +1,15 @@
+import { api } from "utils/api";
+import { formatDatetime } from "utils/formatDateTime";
+import { Snippet } from "./types";
+
+export const getSnippetById = async (id: string) => {
+  const res = await api.get<Snippet>(`/guest/snippet/${id}`);
+  if (!res) {
+    return null;
+  }
+  return {
+    ...res.data,
+    createdAt: formatDatetime(res.data.createdAt),
+    updatedAt: formatDatetime(res.data.updatedAt),
+  };
+};
