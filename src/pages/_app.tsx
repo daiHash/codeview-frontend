1f29e101c2eeff731eb8803d4489628b893cbafb
Daichi Hashimura <dhashi92@gmail.com>
2021-02-03 03:09:07 +0900

    Add type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import "sanitize.css";
 import "../styles/globals.css";
 import { AppWrapper } from "context";
+import { AppProps } from "next/dist/next-server/lib/router/router";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
       <Component {...pageProps} />
