1158bc335966256505396cfaa79f7d9e3840ba22
Daichi Hashimura <dhashi92@gmail.com>
2021-02-10 01:10:23 +0900

    Add loading icon

---
+++ src/components/Loading/LoadingContent.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { LoadingIcon } from ".";
+
+export const LoadingContent: React.FC<{
+  isLoading: boolean;
+  marginTop?: string;
+}> = ({ children, isLoading, marginTop }) => {
+  return (
+    <Fragment>
+      {isLoading && <LoadingIcon marginTop={marginTop} />}
+      {children}
+    </Fragment>
+  );
+};
