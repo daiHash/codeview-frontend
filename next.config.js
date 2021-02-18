1b9b06e6b264d9fda1a09dd099916e8fffb5d553
Daichi Hashimura <dhashi92@gmail.com>
2021-02-18 22:51:39 +0900

    Fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withImages = require("next-images");
 
 module.exports = withImages({
-  exclude: path.resolve(__dirname, 'src/components/SnippetCard/assets/heart.svg')
+  exclude: path.resolve(
+    __dirname,
+    "src/components/SnippetCard/assets/heart.svg"
+  ),
   webpack(config, options) {
     return config;
   },
