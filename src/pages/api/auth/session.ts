4414e5a3342b0abc539d062be0ef7e9103df6938
Daichi Hashimura <dhashi92@gmail.com>
2021-02-28 21:45:50 +0900

    test

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -3,5 +3,5 @@ import { Response, Request } from "express";
 
 export default (req: Request, res: Response) => {
   res.statusCode = 200;
-  res.json({ session: req.headers });
+  res.json({ session: req.headers, cookies: req.cookies });
 };
