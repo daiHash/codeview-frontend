ba8fba7d1da62bd5cb879e00d043022e50107350
Daichi Hashimura <dhashi92@gmail.com>
2021-03-11 01:22:34 +0900

    Remove next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,13 @@ import "../styles/globals.css";
 import { AppWrapper } from "context";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { FocusEffectManager } from "components/Modal/FocusEffectManager";
-import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <AppWrapper>
-        <FocusEffectManager />
-        <Component {...pageProps} />
-      </AppWrapper>
-    </Provider>
+    <AppWrapper>
+      <FocusEffectManager />
+      <Component {...pageProps} />
+    </AppWrapper>
   );
 }
 
